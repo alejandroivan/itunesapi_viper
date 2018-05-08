cb67d09d9595cecf87fdcbced3090d8b254d4b70
Alejandro Iván <alejandro@ratiboo.cl>
2018-05-08 16:04:28 -0300

    Agregado valor resultsPerPage para paginación en iTunes API.

--- itunesapi/Common/Constants.swift
+++ itunesapi/Common/Constants.swift
@@ -3,5 +3,6 @@ import Foundation
 struct Constants {
     struct API {
         static let baseURL = URL(string: "https://itunes.apple.com/search?mediaType=music&limit=20")!
+        static let resultsPerPage = 20
     }
 }
