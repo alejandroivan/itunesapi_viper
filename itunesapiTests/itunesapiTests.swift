3113208fb60257171124dfb8582f087af8b3e756
Alejandro Iván <alejandroivan@icloud.com>
2018-05-09 23:03:09 -0300

    Agregados tests unitarios para Core Data (debería haber más pero la arquitectura delegate utilizada (successFetching... failureFetching...) no lo permite. Deberían ser completionHandlers, pero el tiempo apremia.

--- itunesapiTests/itunesapiTests.swift
+++ itunesapiTests/itunesapiTests.swift
@@ -10,10 +10,12 @@ import XCTest
 @testable import itunesapi
 
 class itunesapiTests: XCTestCase {
+    var searchInteractor: SearchInteractor!
     
     override func setUp() {
         super.setUp()
         // Put setup code here. This method is called before the invocation of each test method in the class.
+        searchInteractor = SearchInteractor()
     }
     
     override func tearDown() {
@@ -21,15 +23,45 @@ class itunesapiTests: XCTestCase {
         super.tearDown()
     }
     
-    func testExample() {
+    func testLocalSearch() {
         // This is an example of a functional test case.
         // Use XCTAssert and related functions to verify your tests produce the correct results.
-    }
-    
-    func testPerformanceExample() {
-        // This is an example of a performance test case.
-        self.measure {
-            // Put the code you want to measure the time of here.
+        let medias = [
+            Media(wrapperType: "song", artistName: "artistName", collectionId: 1, collectionName: "collectionName", kind: "kind", trackId: 1, trackName: "trackName", trackNumber: 1, artwork: "http://www.google.com", previewUrl: "http://www.google.com")
+        ]
+        
+        // Guardar como resultado local
+        do {
+            let jsonData = try JSONEncoder().encode(medias)
+            if let jsonString = String(data: jsonData, encoding: .utf8) {
+                searchInteractor.saveLocalResults(for: "google", json: jsonString)
+            }
+        } catch {
+            print("No se pudo guardar el json.")
+        }
+        
+        // Obtener resultados locales y evaluar desde ellos
+        let results = searchInteractor.fetchLocalResults(for: "google")
+        XCTAssert(results.count == 1, "El número de resultados para \"google\" es incorrecto.")
+        
+        let first = results.first
+        XCTAssertNotNil(first, "No se pudo obtener el resultado de \"google\".")
+        
+        if let m = first {
+            XCTAssert(m.wrapperType == "song")
+            XCTAssert(m.artistName == "artistName")
+            XCTAssert(m.collectionId! == 1)
+            XCTAssert(m.collectionName! == "collectionName")
+            XCTAssert(m.kind! == "kind")
+            XCTAssert(m.trackId! == 1)
+            XCTAssert(m.trackName! == "trackName")
+            XCTAssert(m.trackNumber! == 1)
+            XCTAssert(m.artwork! == "http://www.google.com")
+            XCTAssert(m.previewUrl! == "http://www.google.com")
+            XCTAssertNotNil(m.urlForArtwork)
+            XCTAssertNotNil(m.urlForPreview)
+            XCTAssert(m.urlForArtwork!.absoluteString == "http://www.google.com")
+            XCTAssert(m.urlForPreview!.absoluteString == "http://www.google.com")
         }
     }
     
